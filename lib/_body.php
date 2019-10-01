6fe09b90b6d6b50a9a85638239b6bee0d37c3f42
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2019-10-01 19:20:38 +1000

    Update _body.php
    We are only going to use/support the newer function body_open

--- lib/_body.php
+++ lib/_body.php
@@ -6,26 +6,4 @@ function wp_tracking_body_open() {
     $body_tags = $options['wp_tracking_after_body'];
     echo $body_tags;
 }
-
-// Old way of adding code
-function start_wp_tracking_body(){
-    ob_start( 'wp_tracking_body' );
-}
-    
-function wp_tracking_body($buffer){
-    $options = get_option( 'wp_tracking_settings' );
-    $body_tags = $options['wp_tracking_after_body'];
-
-    if($body_tags){
-        $body = '';
-        $body .= '<body '.$classes.'>';
-        $body .= $body_tags;
-        return preg_replace( '#<body.+>#', $body, $buffer);
-    }
-}
-
-if ( ! function_exists( 'wp_body_open' ) ) {
-    add_action( 'init', 'start_wp_tracking_body' );
-} else {
-    add_action( 'wp_body_open', 'wp_tracking_body_open' );
-}
+add_action( 'wp_body_open', 'wp_tracking_body_open' );
